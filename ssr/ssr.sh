d74cdebc65d6a0a4235d7c5e366bc629fa70d84d
xkolgajah <101954376+wisnucokrosatrio@users.noreply.github.com>
2022-04-04 07:30:18 +0700

    Update ssr.sh

--- ssr/ssr.sh
+++ ssr/ssr.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 
 # Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/wisnucokrosatrio/wisnucokrosatrio/main/ssr"
+wisnuvpn="raw.githubusercontent.com/wisnucokrosatrio/shanum/main/ssr"
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
