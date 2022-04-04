8e9a37f76b048adab0d4bb695b155571f298feb3
xkolgajah <101954376+wisnucokrosatrio@users.noreply.github.com>
2022-04-04 07:59:20 +0700

    Update delexp.sh

--- ssh/delexp.sh
+++ ssh/delexp.sh
@@ -13,18 +13,6 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/AkbarStoreVPN/perizinan/main/ipvps.txt | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/lis.tio.718"
-echo -e "${NC}${LIGHT}WhatsApp : 081545854516"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/Akbar218"
-exit 0
-fi
 clear
                hariini=`date +%d-%m-%Y`
                echo "Thank you for removing the EXPIRED USERS"
