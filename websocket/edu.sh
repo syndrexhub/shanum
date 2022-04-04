76c2cf8435ebdfface007c7c3d6ae9970c1fd297
xkolgajah <101954376+wisnucokrosatrio@users.noreply.github.com>
2022-04-04 07:33:06 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 # Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/wisnucokrosatrio/wisnucokrosatrio/main/websocket"
+wisnuvpn="raw.githubusercontent.com/wisnucokrosatrio/shanum/main/websocket"
 
 # Getting Proxy Template
 wget -q -O /usr/local/bin/ws-nontls https://${wisnuvpn}/ws-nontls.py
