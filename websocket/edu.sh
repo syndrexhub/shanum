d193ce31a2a3e2d36a14ea8427568e22d39db473
xkolgajah <101954376+wisnucokrosatrio@users.noreply.github.com>
2022-04-06 01:48:30 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -34,7 +34,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 2052
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 80
 Restart=on-failure
 
 [Install]
@@ -90,7 +90,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 70
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
 Restart=on-failure
 
 [Install]
