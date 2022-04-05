31216ae725578650e0071ef42f50c72b6f5dc6a0
xkolgajah <101954376+wisnucokrosatrio@users.noreply.github.com>
2022-04-05 12:02:55 +0700

    Update addssh.sh

--- ssh/addssh.sh
+++ ssh/addssh.sh
@@ -11,8 +11,6 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
-# Getting
-
 domain=$(cat /etc/xray/domain)
 clear
 read -p "Username : " Login
