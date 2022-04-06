ee349672a6bc216882f2c349e3467c9c24891c74
xkolgajah <101954376+wisnucokrosatrio@users.noreply.github.com>
2022-04-06 10:44:03 +0700

    Update ins-ssrust.sh

--- shadowsocks/ins-ssrust.sh
+++ shadowsocks/ins-ssrust.sh
@@ -71,6 +71,18 @@ ExecStart=/usr/local/bin/ssserver -c /etc/shadowsocks-rust/config.json
 WantedBy=multi-user.target
 EOF
 
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 50002 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 50002 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 50203 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 50203 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2081 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2081 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2082 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2082 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2083 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2083 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+ip6tables-save > /etc/ip6tables.up.rules
 systemctl daemon-reload
 systemctl stop ssws.service
 systemctl start ssws.service
