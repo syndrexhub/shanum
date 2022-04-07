0aee4753455624a05f2e7ee05e014fee0ce9a910
xkolgajah <101954376+wisnucokrosatrio@users.noreply.github.com>
2022-04-07 18:15:12 +0700

    Update ws-tls.py

--- websocket/ws-tls.py
+++ websocket/ws-tls.py
@@ -5,14 +5,14 @@ import socket, threading, thread, select, signal, sys, time, getopt
 LISTENING_ADDR = '0.0.0.0'
 LISTENING_PORT = sys.argv[1]
 else
-LISTENING_PORT = 80
+LISTENING_PORT = 8443
 # Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:8443'
+DEFAULT_HOST = '127.0.0.1:443'
 RESPONSE = 'HTTP/1.1 101 <b><font color="red"> WISNU COKRO SATRIO </font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 class Server(threading.Thread):
     def __init__(self, host, port):
