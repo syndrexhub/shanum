d54ecc4725b86ba3649e3c67dd4b23233f24a9a6
xkolgajah <101954376+wisnucokrosatrio@users.noreply.github.com>
2022-04-08 12:18:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -147,8 +147,8 @@ echo " 🔰 TROJAN H2C : 443" |tee -a log-install.txt
 echo " 🔰 TROJAN TCP : 4443" |tee -a log-install.txt
 echo " 🔰 TROJAN WS TLS : 2096" |tee -a log-install.txt
 echo " 🔰 TROJAN WS NON TLS : 2095" |tee -a log-install.txt
-echo " 🔰 SOCKS5 WEBSOCKET : 2096" |tee -a log-install.txt
-echo " 🔰 SOCK5 WEBSOCKET : 8880" |tee -a log-install.txt
+echo " 🔰 SOCKS5 WEBSOCKET : 2083" |tee -a log-install.txt
+echo " 🔰 XRAY SHADOWSOCKS : 333" |tee -a log-install.txt
 echo " 🔰 XRAY MT PROTO : 111" |tee -a log-install.txt
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo " 🔰>> Server Information & Other Features"                  |tee -a log-install.txt
